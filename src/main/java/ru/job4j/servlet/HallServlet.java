409c2de62fc08a36b0fed1aa3eb32a0c937d9390
Lyubov <n.lubovj@gmail.com>
2023-12-06 23:59:30 +0300

    Add jackson, correct project

--- src/main/java/ru/job4j/servlet/HallServlet.java
+++ src/main/java/ru/job4j/servlet/HallServlet.java
@@ -1,19 +1,39 @@
 package ru.job4j.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.job4j.model.Seat;
+import ru.job4j.service.Service;
+import ru.job4j.service.TicketService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class HallServlet extends HttpServlet {
+    private final Service<Seat> service = TicketService.instOf();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("application/json; charset=utf-8");
+        List<Seat> seats = (List<Seat>) service.findAll();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String stringOfSeats = objectMapper.writeValueAsString(seats);
+        resp.getWriter().write(stringOfSeats);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        resp.setContentType("application/json; charset=utf-8");
+        int seatId = Integer.parseInt(req.getReader().readLine());
+        Seat seat = service.findById(seatId);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(seat);
+        PrintWriter writer = resp.getWriter();
+        writer.print(json);
+        writer.flush();
     }
 }
